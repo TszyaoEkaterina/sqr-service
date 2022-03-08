97d6fa3f62b0b520376f2f1f19f4c39254210b46
Ekaterina <3022805283@qq.com>
2022-03-08 22:41:28 +0800

    sqr service finished

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,18 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public long calculate(long leftBorder, long rightBorder) {
+        int i = 0;
+        for (int number = 10; number < 100; number++) {
+            if (number * number <= rightBorder) {
+                if (number * number >= leftBorder) {
+                    i = i + 1;
+                }
+            }
+        }
+        return i;
+
+    }
+
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest1.java
@@ -0,0 +1,20 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest1 {
+
+    @ParameterizedTest
+    @CsvSource(value = {"range under10*10,0,99,0",
+            "range from 200 to 300,200,300,3",
+            "range from 400 to 500,400,500,3",
+            "range over99*99,9802,10000,0"})
+    void shouldCalculate(String testName, long leftborder, long rightborder, long expected) {
+        SQRService service = new SQRService();
+        long actual = service.calculate(leftborder, rightborder);
+        assertEquals(actual, expected);
+    }
+}
