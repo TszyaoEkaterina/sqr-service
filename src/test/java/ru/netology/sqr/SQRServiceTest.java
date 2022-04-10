27ff8d3c929c2e29ffd22cc24bfc3e46d58eaaca
Ekaterina <3022805283@qq.com>
2022-04-10 21:01:15 +0800

    CsvFileSource

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"range under10*10,0,99,0",
-            "range from 200 to 300,200,300,3",
-            "range from 400 to 500,400,500,3",
-            "range over99*99,9802,10000,0"})
+    @CsvFileSource(resources = "/data.csv")
     void shouldCalculate(String testName, long leftBorder, long rightBorder, long expected) {
         SQRService service = new SQRService();
 
